677c271c02351b07d446ef0742d771c7bc5a1ef7
Peter Tran <petertranminh22@gmail.com>
2023-05-10 18:10:06 -0400

    update

---
+++ src/lib/graphic.svelte
@@ -0,0 +1,11 @@
+<div class="graphic-container">
+  <img id="graphic" src="icons/home-graphic.svg">
+</div>
+
+
+<style>
+  #graphic {
+    position: relative;
+    margin: 0% 10%;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script>
   import { fly } from 'svelte/transition';
+  import Graphic from '../lib/graphic.svelte';
 </script>
 <link rel="stylesheet" href="styles/index.css" />
 <main in:fly="{{ y: 20, duration: 500}}">
-  <div class="intro">
+  <div class="intro">    
     <h1>Hi, my name is Peter.</h1>
     <p class="paragraph">
       I’m a <b>computer science</b> junior at the Toronto Metropolitan
@@ -13,12 +14,14 @@
       <b>JavaScript</b>, but I’m always trying to add a new one to the list. Check out
       some of my projects and photographs I have taken over the years :)
     </p>
-  </div>
-  <div class="socials">
-    <p>my socials</p>
-    <a href="https://github.com/ptktran" target="_blank">github</a>
-    <a href="https://www.linkedin.com/in/peter-tran-95a4a922b/" target="_blank">linkedin</a>
-    <a href="https://www.instagram.com/ptktran/" target="_blank">instagram</a>
-    <a href="https://open.spotify.com/user/m9l81amepbe96a1owslqu2ytk?si=8b26a1a1523e4b12" target="_blank">spotify</a>
+    <div class="socials">
+      <p>my socials</p>
+      <a href="https://github.com/ptktran" target="_blank">github</a>
+      <a href="https://www.linkedin.com/in/peter-tran-95a4a922b/" target="_blank">linkedin</a>
+      <a href = "mailto: petertranminh22@gmail.com" target="_blank">email</a>
+      <a href="https://drive.google.com/file/d/1X-oaftiPX32a7Z5cFbBDaHMNlZYKcSug/view" target="_blank">resume</a>
+      <a href="https://open.spotify.com/user/m9l81amepbe96a1owslqu2ytk?si=8b26a1a1523e4b12" target="_blank">spotify</a>
+      <!-- <a href="https://www.instagram.com/ptktran/" target="_blank">instagram</a> -->
+    </div>
   </div>
 </main>
