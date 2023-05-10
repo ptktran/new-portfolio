39ecf71b074189b28f2b8a04bce0c92bc718fbdc
Peter Tran <petertranminh22@gmail.com>
2023-05-10 02:59:52 -0400

    fix

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.eb0faad2.js","_app/immutable/chunks/index.68a96b34.js"];
-export const stylesheets = [];
-export const fonts = [];
