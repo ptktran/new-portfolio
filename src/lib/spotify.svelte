724c611cd3dc443c92fb601ae20cd60182b22264
Peter Tran <petertranminh22@gmail.com>
2023-05-11 02:11:47 -0400

    test

--- src/lib/spotify.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  export let track;
-</script>
-
-<div>
-  <h2>Currently playing:</h2>
-  <p>{track.name} by {track.artists[0].name}</p>
-  <iframe src={`https://open.spotify.com/embed/track/${track.id}`} width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,5 @@
 <script>
   import { fly } from 'svelte/transition';
-  import SpotifyWebApi from 'spotify-web-api-js';
-  import { onMount } from 'svelte';
-  import Spotify from '../lib/spotify.svelte';
-
-  const spotifyApi = new SpotifyWebApi({
-    clientId: 'fef82cf191c541929a78fe3860c01272',
-    clientSecret: '5bb050b8f77a4f8781bbf1e3cc3b1742',
-  });
-
-  let track = null;
-
-  onMount(async () => {
-    const { access_token } = await spotifyApi.clientCredentialsGrant();
-    spotifyApi.setAccessToken(access_token);
-    const response = await spotifyApi.getMyCurrentPlayingTrack();
-    track = response.item;
-    console.log(track);
-  });
 </script>
 
 <link rel="stylesheet" href="styles/index.css" />
@@ -41,10 +23,5 @@
       <a href="https://open.spotify.com/user/m9l81amepbe96a1owslqu2ytk?si=8b26a1a1523e4b12" target="_blank">spotify</a>
       <!-- <a href="https://www.instagram.com/ptktran/" target="_blank">instagram</a> -->
     </div>
-    {#if track}
-      <Spotify {track} />
-    {:else}
-      <p>No track currently playing</p>
-    {/if}
   </div>
 </main>
