e81187ac473f39ce9f5a743aa3115f33b78c9a93
Peter Tran <petertranminh22@gmail.com>
2023-05-11 02:31:22 -0400

    update

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -5,24 +5,24 @@
 <main>
   <div class="gallery-row">
     <div class="col" in:fly="{{ y: 20, duration: 100}}">
-        <img src="./photos/p1.jpg">
-        <img src="./photos/p2.jpg">
-        <img src="./photos/p3.jpg">
+        <img src="./photos/p1.jpg" alt="photography">
+        <img src="./photos/p2.jpg" alt="photography">
+        <img src="./photos/p3.jpg" alt="photography">
     </div>
     <div class="col" in:fly="{{ y: 20, duration: 300}}">
-        <img src="./photos/p4.jpg">
-        <img src="./photos/p5.jpg">
-        <img src="./photos/p6.jpg">
+        <img src="./photos/p4.jpg" alt="photography">
+        <img src="./photos/p5.jpg" alt="photography">
+        <img src="./photos/p6.jpg" alt="photography">
     </div>
     <div class="col" in:fly="{{ y: 20, duration: 500}}">
-        <img src="./photos/p7.jpg">
-        <img src="./photos/p8.jpg">
-        <img src="./photos/p9.jpg">
+        <img src="./photos/p7.jpg" alt="photography">
+        <img src="./photos/p8.jpg" alt="photography">
+        <img src="./photos/p9.jpg" alt="photography">
     </div>
     <div class="col" in:fly="{{ y: 20, duration: 700}}">
-        <img src="./photos/p10.jpg">            
-        <img src="./photos/p11.jpg">
-        <img src="./photos/p12.jpg">
+        <img src="./photos/p10.jpg" alt="photography">            
+        <img src="./photos/p11.jpg" alt="photography">
+        <img src="./photos/p12.jpg" alt="photography">
     </div>
   </div>
 </main>
